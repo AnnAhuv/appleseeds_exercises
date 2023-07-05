25e53b156105ff60afe80dd13cfbf8adaf2513f0
AnnAhuv <anna.ag1203@gmail.com>
2023-07-06 00:19:03 +0300

    ex 1.1

---
+++ js/primitive_data_types.js
@@ -0,0 +1,15 @@
+let message = "You enter a dimly lit room.";
+let treasureCount = 1;
+let key = 74856300927548150n;
+let isTrapActivated = false;
+let secretPassage = null;
+let mysteriousArtifact;
+let ancientSymbol = Symbol("ancient symbol");
+
+console.log(typeof message);
+console.log(typeof treasureCount);
+console.log(typeof key);
+console.log(typeof isTrapActivated);
+console.log(typeof secretPassage);
+console.log(typeof mysteriousArtifact);
+console.log(typeof ancientSymbol);
