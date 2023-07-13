7c67dedbe396144a516928dfe63c4ac11f4251f9
AnnAhuv <anna.ag1203@gmail.com>
2023-07-13 15:33:53 +0300

    reorganization js ex

--- js/country_to_live_in.js
+++
@@ -1,9 +0,0 @@
-function countryToLiveIn(language, isIsland, population, country) {
-  if(language === 'English' && !isIsland && population < 50000000) {
-    console.log(`You should consider living in ${country}`);
-  } else {
-    console.log(`${country} does not meet your criteria`);
-  }
-}
-
-console.log(countryToLiveIn('English', false, 2560000, 'theRightCountry'));
